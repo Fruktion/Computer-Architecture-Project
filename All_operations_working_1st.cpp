31b27cac579a12d89884b5932b6b38d86984cfda
Fruktion <106382628+Fruktion@users.noreply.github.com>
2023-05-24 13:06:34 +0200

    Update All_operations_working_1st.cpp

--- All_operations_working_1st.cpp
+++ All_operations_working_1st.cpp
@@ -210,87 +210,87 @@ public:
 
     BigNumber operator/(const BigNumber& other) const {
 
-    if (other == BigNumber("0")) {
+        if (other == BigNumber("0")) {
 
-        throw std::invalid_argument("Division by zero");
-
-    }
-
-    BigNumber dividend(*this);
-
-    BigNumber divisor(other);
-
-    dividend.isNegative = false;
-
-    divisor.isNegative = false;
-
-    
-
-    // Shift dividend and divisor left by the number of decimal places of the divisor
-
-    int shift = other.decimalPoint;
-
-    for(int i = 0; i < shift; i++){
-
-        dividend = dividend * BigNumber("10");
-
-        divisor = divisor * BigNumber("10");
-
-    }
-
-    BigNumber quotient("0");
-
-    BigNumber remainder("0");
-
-    int precision = 10;  // Set the precision you want
-
-    for (int i = dividend.digits.size() - 1; i >= -precision; --i) {
-
-        remainder = remainder * BigNumber("10") + BigNumber(std::to_string(dividend.getDigit(i)));
-
-        int count = 0;
-
-        while (remainder >= divisor) {
-
-            remainder = remainder - divisor;
-
-            ++count;
+            throw std::invalid_argument("Division by zero");
 
         }
 
-        quotient = quotient * BigNumber("10") + BigNumber(std::to_string(count));
+        BigNumber dividend(*this);
+
+        BigNumber divisor(other);
+
+        dividend.isNegative = false;
+
+        divisor.isNegative = false;
+
+
+
+        // Shift dividend and divisor left by the number of decimal places of the divisor
+
+        int shift = other.decimalPoint;
+
+        for(int i = 0; i < shift; i++){
+
+            dividend = dividend * BigNumber("10");
+
+            divisor = divisor * BigNumber("10");
+
+        }
+
+        BigNumber quotient("0");
+
+        BigNumber remainder("0");
+
+        int precision = 1000;  // Set the precision you want
+
+        for (int i = dividend.digits.size() - 1; i >= -precision; --i) {
+
+            remainder = remainder * BigNumber("10") + BigNumber(std::to_string(dividend.getDigit(i)));
+
+            int count = 0;
+
+            while (remainder >= divisor) {
+
+                remainder = remainder - divisor;
+
+                ++count;
+
+            }
+
+            quotient = quotient * BigNumber("10") + BigNumber(std::to_string(count));
+
+        }
+
+        quotient.isNegative = isNegative != other.isNegative;
+
+        quotient.removeLeadingZeros();
+
+        // Adjust the decimal point position immediately after division
+
+        quotient.decimalPoint = quotient.digits.size() - decimalPoint + other.decimalPoint - shift;
+
+        // Rounding
+
+        if (quotient.getDigit(-precision - 1) >= 5) {  // Check the next digit after precision
+
+            BigNumber round("1" + std::string(precision - 1, '0'));  // Fixed the rounding issue here
+
+            quotient = quotient + round;
+
+        }
+
+        while(quotient.digits.back() == 0 && quotient.decimalPoint > 1){  // To remove leading zeros
+
+            quotient.digits.pop_back();
+
+            quotient.decimalPoint--;
+
+        }
+
+        return quotient;
 
     }
-
-    quotient.isNegative = isNegative != other.isNegative;
-
-    quotient.removeLeadingZeros();
-
-    // Adjust the decimal point position immediately after division
-
-    quotient.decimalPoint = quotient.digits.size() - decimalPoint + other.decimalPoint - shift; 
-
-    // Rounding
-
-    if (quotient.getDigit(-precision - 1) >= 5) {  // Check the next digit after precision
-
-        BigNumber round("1" + std::string(precision - 1, '0'));  // Fixed the rounding issue here
-
-        quotient = quotient + round;
-
-    }
-
-    while(quotient.digits.back() == 0 && quotient.decimalPoint > 1){  // To remove leading zeros
-
-        quotient.digits.pop_back();
-
-        quotient.decimalPoint--;
-
-    }
-
-    return quotient;
-
-}
 
     std::string toString() const {
 
@@ -460,9 +460,9 @@ int main() {
 
     // Test addition
 
-    BigNumber num1("123.45");
+    BigNumber num1("3141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844.609550582231725359408128481117450284102701938521105559644622948954930381964428810975665933446128475648233786783165271201909145648566923460348610454326648213393607260249141273724587006606315588174881520920962829254091715364367892590360011330530548820466521384146951941511609433057270365759591953092186117381932611793105118548074462379962749567351885752724891227938183011949129833673362440656643086021394946395224737190702179860943702770539217176293176752384674818467669405132000568127145263560827785771342757789609173637178721468440901224953430146549585371050792279689258923542019956112129021960864034418159813629774771309960518707211349999998372978049951059731732816096318595024459455346908302642522308253344685035261931188171010003137838752886587533208381420617177669147303598253490428755468731159562863882353787593751957781857780532171226806613001927876611195909216420199");
 
-    BigNumber num2("-67.89");
+    BigNumber num2("-3141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844.609550582231725359408128481117450284102701938521105559644622948954930381964428810975665933446128475648233786783165271201909145648566923460348610454326648213393607260249141273724587006606315588174881520920962829254091715364367892590360011330530548820466521384146951941511609433057270365759591953092186117381932611793105118548074462379962749567351885752724891227938183011949129833673362440656643086021394946395224737190702179860943702770539217176293176752384674818467669405132000568127145263560827785771342757789609173637178721468440901224953430146549585371050792279689258923542019956112129021960864034418159813629774771309960518707211349999998372978049951059731732816096318595024459455346908302642522308253344685035261931188171010003137838752886587533208381420617177669147303598253490428755468731159562863882353787593751957781857780532171226806613001927876611195909216420199");
 
     BigNumber sum = num1 + num2;
 
@@ -470,25 +470,25 @@ int main() {
 
     // Test subtraction
 
-    BigNumber num3("-1000.1234");
+    BigNumber num3(num1);
 
-    BigNumber num4("2000.9876");
+    BigNumber num4(num1);
 
     BigNumber diff = num3 - num4;
 
     std::cout << num3.toString() << " - " << num4.toString() << " = " << diff.toString() << std::endl;
 
-    BigNumber num5("12.34");
+    BigNumber num5(num1);
 
-    BigNumber num6("56.78");
+    BigNumber num6(num1);
 
     BigNumber product = num5 * num6;
 
     std::cout << num5.toString() << " * " << num6.toString() << " = " << product.toString() << std::endl;
 
-    BigNumber num7("1234.56");
+    BigNumber num7(num1);
 
-    BigNumber num8("78.9");
+    BigNumber num8(num1);
 
     BigNumber quotient = num7 / num8;
 
